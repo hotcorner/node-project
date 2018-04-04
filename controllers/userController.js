49ba6d57e1510ba0816005b4921c032fdd2054c4
Clint <clintonia@gmail.com>
2018-04-04 01:23:20 -0400

    user reg promise then() for duplicate email attempts

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,13 +36,13 @@ exports.register = async (req, res, next) => {
 	const user = new User({ name: req.body.name, email: req.body.email });
 	const register = promisify(User.register, User);
 	await register(user, req.body.password)
-		.then(
-			function fulfilled(){
-				next();
-			},
-			function rejected(err){
-				req.flash('error', 'That email address is already being used.');
-				res.render('register', {title: 'Register', flashes: req.flash()});
-			}
-		);
+	.then(
+		function fulfilled(){
+			next();
+		},
+		function rejected(err){
+			req.flash('error', 'That email address is already being used.');
+			res.render('register', {title: 'Register', flashes: req.flash()});
+		}
+	);
 }
