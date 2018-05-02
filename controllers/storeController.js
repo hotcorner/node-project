78c305fcd3fd8770f3e56a41047a532fe84f5312
Rowden <crowden@ea.com>
2018-05-02 12:10:42 -0400

    Video 29: added permissions on stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -39,6 +39,7 @@ exports.resize = async (req, res, next) => {
 }
 
 exports.createStore = async (req, res) => {
+	req.body.author = req.user._id;
 	const store = await (new Store(req.body)).save();
 	req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
 	res.redirect(`/store/${store.slug}`);
@@ -50,8 +51,15 @@ exports.getStores = async (req, res) => {
 	res.render('stores', {stores, title: 'Stores'});
 }
 
+const confirmOwner = (store, user) => {
+	if (!store.author.equals(user._id)){
+		throw Error('You must own a store to edit it!');
+	}
+}
+
 exports.editStore = async (req, res) => {
 	const store = await Store.findOne({ _id: req.params.id });
+	confirmOwner(store, req.user);
 	res.render('editStore', {store, title: `Edit ${store.name}`});
 }
 
@@ -72,7 +80,7 @@ exports.updateStore = async (req, res) => {
 exports.viewStore = async (req, res, next) => {
 	const store = await Store.findOne({
 		slug: req.params.slug,
-	});
+	}).populate('author');
 	if(!store) return next();
 	res.render('store', {title:store.name, store});
 }
