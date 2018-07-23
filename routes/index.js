e44cc104ffa9b4a0b4775ca060d84942850511a6
Clint Rowden <crowden@ea.com>
2018-07-23 15:43:16 -0400

    adding hearts

--- routes/index.js
+++ routes/index.js
@@ -41,4 +41,5 @@ router.post('/account/reset/:token', authController.confirmPasswords, catchError
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
 router.get('/map', storeController.mapPage);
+router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
 module.exports = router;
